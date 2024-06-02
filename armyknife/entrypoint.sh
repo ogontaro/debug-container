cd49a91933f97fa503e805b4097f64432c7edb41
ogontaro <ogontaro@gmail.com>
2024-06-02 22:27:32 +0900

    update armyknife

--- armyknife/entrypoint.sh
+++ armyknife/entrypoint.sh
@@ -3,10 +3,7 @@
 # Execute all .sh files in the /opt/armyknife/init directory
 for script in /opt/armyknife/init/*.sh; do
   if [ -f "$script" ]; then
-    echo "Executing $script..."
     bash "$script"
-  else
-    echo "No .sh files found in /opt/armyknife/init directory."
   fi
 done
 
--- armyknife_slim/init-scripts/0_setup_fish.sh
+++ armyknife_slim/init-scripts/0_setup_fish.sh
@@ -7,7 +7,7 @@ cat <<EOF >>~/.config/fish/config.fish
 source /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish
 if status is-interactive
     fzf_key_bindings
-    bind \cf fzf-cd-widget
+    bind \cg fzf-cd-widget
 end
 export FZF_CTRL_T_COMMAND='rg --files --hidden --follow -uu'
 export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
