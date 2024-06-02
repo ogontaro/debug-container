dd7c6317be29ba7c497fa3ffc96c079fc6f6caf6
ogontaro <ogontaro@gmail.com>
2024-06-02 22:34:43 +0900

    update armyknife

--- armyknife/init-scripts/0_setup_fish.sh
+++ armyknife/init-scripts/0_setup_fish.sh
@@ -8,6 +8,7 @@ source /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish
 if status is-interactive
     fzf_key_bindings
     bind \cg fzf-cd-widget
+    bind "ç" fzf-cd-widget
 end
 export FZF_CTRL_T_COMMAND='rg --files --hidden --follow -uu'
 export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
